bef6de85382388f83469367be6edd27925bcb70c
Sherzod Artikbayev <info@web-pro.uz>
2026-05-29 01:36:05 +0500

    feat: Tasks app

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "tasks"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet")
+    path("add", views.add, name="add")
 ]
